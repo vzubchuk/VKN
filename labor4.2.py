e259468f100be1cc8f6aa07a8236503557949a23
vzubchuk <113578623+vzubchuk@users.noreply.github.com>
2022-09-20 13:21:39 +0300

    Update labor4.2.py

--- labor4.2.py
+++ labor4.2.py
@@ -1,7 +1,10 @@
 import math
+def func(x, y, a, t):
+   Z=2,33*math.pi*(x*math.sin(y)+y*math.cos(a))+(math.e**a*t)
+   return(Z)
 x=int(input("Введіть X= "))
 y=int(input("Введіть Y= "))
 a=int(input("Введіть a= "))
 t=int(input("Введіть t= "))
-Z=2,33*math.pi*(x*math.sin(y)+y*math.cos(a))+(math.e**a*t)
-print(Z)
+R=func(x, y, a, t)
+print(R)
